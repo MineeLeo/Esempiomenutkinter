89d7f0847546e5a215becdf4960d5203935e87ee
MineeLeo <124923939+MineeLeo@users.noreply.github.com>
2023-05-18 10:12:22 +0200

    Update menutkinter1.py

--- menutkinter1.py
+++ menutkinter1.py
@@ -11,21 +11,26 @@ class Finestra:
 		self.finestra.mainloop()
         
 	def crea_widgets(self):
-		# creo il frame per il menù
+		# creo il frame per il menu
 		menubar = tk.Menu(self.finestra)
         
 		# creo la voce di menù "File" con due sottomenu
+		# Prima devo creare le voci che voglio mettere all'interno del menu e successivamente aggiungo la cascata
 		filemenu = tk.Menu(menubar, tearoff=0)
-		filemenu.add_command(label="Apri", command=self.apri_file)
-		filemenu.add_command(label="Salva", command=self.salva_file)
+		filemenu.add_command(label="Apri", command=self.apri_file, activebackground="Green", background="Blue")
+		filemenu.add_command(label="Salva", command=self.salva_file, activebackground="Green", background="Blue")
 		filemenu.add_separator()
-		filemenu.add_command(label="Esci", command=self.finestra.quit)
+		filemenu.add_command(label="Esci", command=self.finestra.quit, activebackground="Red", background="Blue")
 		menubar.add_cascade(label="File", menu=filemenu)
         
 		# creo la voce di menù "Aiuto" con un sottomenu
 		helpmenu = tk.Menu(menubar, tearoff=0)
-		helpmenu.add_command(label="Info", command=self.info)
+		helpmenu.add_command(label="Info", command=self.info , activebackground="Green", background="Blue")
 		menubar.add_cascade(label="Aiuto", menu=helpmenu)
+		menubar.add_radiobutton(label="radio1")
+		menubar.add_radiobutton(label="radio2")
+		menubar.add_checkbutton(label="check1")
+		menubar.add_checkbutton(label="check2")
         
 		# configuro la finestra per usare il menù appena creato
 		self.finestra.config(menu=menubar)
